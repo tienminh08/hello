37b0a306c750fb72d0f7ae39f6b066945ac44365
Tien Minh 08 <tanktan1212@gmail.com>
2022-10-04 00:32:43 +0700

    first commit

---
+++ lol.cpp
@@ -0,0 +1,22 @@
+int main() {
+import math;
+a=float(input("nhap a="));
+b=float(input("nhap b="));
+c=float(input("nhap c="));
+if a=0:
+	a=float(input("nhap lai a=",a));
+	else:
+		delta=sqr(b)-4*a*c;
+		if deltal=0:
+			kep= (-b)/(2*a);
+			print("phuong trinh co nghiem kep =",kep);
+			else delta>0:
+				x1=(-b+sqrt(delta))/(2*a);
+				x2=(-b-sqrt(delta))/(2*a);
+				print("phuong trinh co hai nghiem x1=",x1);
+				print("x2=",x2);
+				else delta<0:
+					print("phuong trinh vo nghiem");
+}
+
+
